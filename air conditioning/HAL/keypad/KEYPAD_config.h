b1d6a0c9d140a9418809b27a70ab93541169f402
Anas <67676102+AnasMahmoud99@users.noreply.github.com>
2023-04-18 18:21:20 +0200

    Update KEYPAD_config.h

--- air conditioning/HAL/keypad/KEYPAD_config.h
+++ air conditioning/HAL/keypad/KEYPAD_config.h
@@ -10,8 +10,27 @@
 #ifndef KEYPAD_CONFIG_H_
 #define KEYPAD_CONFIG_H_
 
+#include"../../MCAL/DIO_interface.h"
 
+/******** Columns Definition *********/
+#define COL_1  DIO_PIN0
+#define COL_2  DIO_PIN1
+#define COL_3  DIO_PIN2
 
+/******** Rows Definition *********/
+#define ROW_1  DIO_PIN3
+#define ROW_2  DIO_PIN4
+#define ROW_3  DIO_PIN5
 
+/******** Buttons Definition ********/
+BUTTON1    1
+BUTTON2    2
+BUTTON3    3
+BUTTON4    4
+BUTTON5    5
+BUTTON6    6
+BUTTON7    7
+BUTTON8    8
+BUTTON9    9
 
-#endif /* ADC_CONFIG_H_ */
+#endif /* ADC_CONFIG_H_ */p;'
