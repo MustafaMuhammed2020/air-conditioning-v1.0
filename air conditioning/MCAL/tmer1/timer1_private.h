5e8bcfe51bc176326ab4dc314ff177a2b2832e90
Mustafa MOhammed <mustafa mohammed0321@gmail.com>
2023-04-19 23:45:04 +0200

    ADC driver updated

--- air conditioning/MCAL/tmer1/timer1_private.h
+++ air conditioning/MCAL/tmer1/timer1_private.h
@@ -12,27 +12,27 @@
 /*** YOU CAN ACCESS EACH BYTE SEPERATELY OR ACESS THE TWO BYTES IN TCNT1 **/
 #define TCNT1H   (* ((volatile uint8_t *)  (0x4D)))
 #define TCNT1L   (* ((volatile uint8_t *)  (0x4C))) 
-#define TCNT1    (* ((volatile uint16t *) (0x4C)))
+#define TCNT1    (* ((volatile uint16_t *) (0x4C)))
 
 /*** YOU CAN ACCESS EACH BYTE SEPERATELY OR ACESS THE TWO BYTES IN OCR1A **/
 #define OCR1AH (* ((volatile uint8_t *)  (0x4B)))
 #define OCR1AL (* ((volatile uint8_t *)  (0x4A)))
-#define OCR1A  (* ((volatile uint16t *) (0x4A)))
+#define OCR1A  (* ((volatile uint16_t *) (0x4A)))
 
 /*** YOU CAN ACCESS EACH BYTE SEPERATELY OR ACESS THE TWO BYTES IN OCR1B **/
 #define OCR1BH  (* ((volatile uint8_t *)   (0x49)))
 #define OCR1BL  (* ((volatile uint8_t *)   (0x48)))
-#define OCR1B   (* ((volatile uint16t *)  (0x48)))
+#define OCR1B   (* ((volatile uint16_t *)  (0x48)))
 
 /*** YOU CAN ACCESS EACH BYTE SEPERATELY OR ACESS THE TWO BYTES IN ICR1 **/
 #define ICR1H   (* ((volatile uint8_t *)    (0x47)))
 #define ICR1L   (* ((volatile uint8_t *)    (0x46)))
-#define ICR1    (* ((volatile uint16t *)   (0x46)))
+#define ICR1    (* ((volatile uint16_t *)   (0x46)))
 
 #define TIFR    (* ((volatile uint8_t *)    (0x58)))
 
 
-#define TIMSK   (* ((volatile uint16t *)   (0x59)))
+#define TIMSK   (* ((volatile uint16_t *)   (0x59)))
 
 #define SREG    (* ((volatile uint8_t *)    (0x5F)))
 
