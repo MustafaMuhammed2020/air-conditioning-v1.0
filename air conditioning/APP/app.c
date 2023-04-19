ed1c302647b8b47f9af237ba6b61b788a02d74f6
Mustafa MOhammed <mustafa mohammed0321@gmail.com>
2023-04-20 01:16:53 +0200

    buzzer driver added to application

--- air conditioning/APP/app.c
+++ air conditioning/APP/app.c
@@ -9,42 +9,44 @@
 #include "../SERVICE/common_macros.h"
 #include "../SERVICE/standard_types.h"
 
-/** INCLUDE MODULES INTERFACE FILES **/
+/** INCLUDE INTERFACE FILES OF LOWER LAYERS **/
 #include "../HAL/lcd/LCD_interface.h"
 #include "../HAL/lcd/LCD_config.h"
 
 #include "../MCAL/dio/DIO_interface.h"
 #include "../MCAL/dio/DIO_private.h"
+
 #include "../MCAL/timer0/TMR0_interface.h"
 #include "../MCAL/timer0/TMR0_config.h"
+
 #include "../MCAL/adc/ADC_interface.h"
 
+#include "../HAL/buzzer/buzzer_interface.h"
 
-
-uint8_t u8_g_bell[8] =   { 0x04, 0x0E, 0x0E, 0x0E, 0x1F, 0x00, 0x04, 0x00};
+uint8_t u8_g_bell[8] =   { 0x04, 0x0E, 0x0E, 0x0E, 0x1F, 0x00, 0x04, 0x00}; /** BELL SHAPE PATTERN **/
 	
-uint8_t u8_g_nobell[8] =   { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+uint8_t u8_g_nobell[8] =   { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; /** EMPTY PATTERN **/
 
-uint32_t u32_a_temp = 0 ;
+uint32_t u32_a_temp = 0 ; /** GLOBAL VARIABLE FOR TEMPERATURE **/
 
 void APP_init()
 {
 
-	ADC_init();
+	ADC_init();  /** INITIALIZE ADC **/
 	
-	TMR0_init();
+	TMR0_init();  /** INITIALIZE TMR0 **/
 	
-	LCD_init();
+	LCD_init();   /** INITIALIZE LCD **/
 	
-	TMR0_delayms(20);
+	TMR0_delayms(20); /** DELAY FOR 20 ms FOR LCD TO BE STABLE BEFORE OPERATE */
 		
-	LCD_writecustomchar(u8_g_bell , 0);
+	LCD_writecustomchar(u8_g_bell , 0); /** STORE THE BELL SHAPE IN CGRAM OF LCD **/
+	 
+	LCD_writecustomchar(u8_g_nobell , 1); /** STORE EMPTY SHAPE IN CGRAM OF LCD **/
 	
-	LCD_writecustomchar(u8_g_nobell , 1);
+	BUZZ_init();  /** INITIALIZE THE BUZZER **/
 	
-	DIO_setpindir(DIO_PORTA , DIO_PIN3 , DIO_PIN_OUTPUT);  /** BUZZER **/
-	
-	DIO_setpinvalue(DIO_PORTA , DIO_PIN3 , DIO_PIN_LOW); /** BUZZER IS OFF **/
+	BUZZ_off();   /** BUZZER IS OFF **/
 	
 }
 
@@ -52,92 +54,34 @@ void APP_init()
 void APP_start()
 {
 		
-	u32_a_temp = ADC_read();
+	u32_a_temp = ADC_read();  /** READ THE VALUE FROM ADC CHANNEL **/
 	
-	LCD_goto(1,11);
+	LCD_goto(1,11); /** DISPLAY THE ADC READ ON LCD **/
 	LCD_writeint(u32_a_temp);
 	
-	if (u32_a_temp > 500)
+	if (u32_a_temp > 500) /** IF ADC READ BIGGER THN 500 **/
 	{
-		LCD_goto(1,2);
-		
+		LCD_goto(1,2); 
 		LCD_writestr("Temp = ");
 		
-		DIO_setpinvalue(DIO_PORTA , DIO_PIN3 , DIO_PIN_HIGH);
+		BUZZ_on(); /** START THE BUZZER INDICATOR **/
 		
-		LCD_goto(0,8);
+		LCD_goto(0,8); /** DRAW BELL SHAPE **/
 		LCD_writechar(0);
 	}
 	
-	else
+	else /** ADC READ LES THAN 500 **/
 	{
 		
 		LCD_goto(1,2);
-		
 		LCD_writestr("Temp = ");
 		
-		DIO_setpinvalue(DIO_PORTA , DIO_PIN3 , DIO_PIN_LOW);
+		BUZZ_off();  /** STOP THE BUZZER **/
 		
-		LCD_goto(0,8);
+		LCD_goto(0,8); /** REMOVE BELL SHAPE **/
 		LCD_writechar(1);
 	}
 	
-//  	LCD_goto(0,8);
-//  	LCD_writechar(0);
-//  	TMR0_delayms(90);
-// 	 
-// 	TMR0_delayms(2000);
-// 	
-// 	LCD_sendcmd(LCD_CLEAR);
-// 	
-// 	TMR0_delayms(20);
-	
-//  	LCD_goto(0,8);
-//  	LCD_writechar(1);
-//  	TMR0_delayms(90);
-//  	
-// 	 LCD_goto(0,8);
-// 	 LCD_writechar(2);
-// 	 TMR0_delayms(90);
-//  	
-// 	 LCD_goto(0,8);
-// 	 LCD_writechar(1);
-// 	 TMR0_delayms(90);
-//  	
-// 	 LCD_goto(0,8);
-// 	 LCD_writechar(0);
-// 	 TMR0_delayms(90);
-// 	 
-// 	 LCD_goto(0,8);
-// 	 LCD_writechar(3);
-// 	 TMR0_delayms(90);
-//  	
-// 	 LCD_goto(0,8);
-// 	 LCD_writechar(4);
-// 	 TMR0_delayms(90);
-//  	
-// 	 LCD_goto(0,8);
-// 	 LCD_writechar(3);
-// 	 TMR0_delayms(90);
-	 	
-// 	LCD_writestr("MUSTAFA MOHAMMED");
-// 	
-// 	LCD_goto(1 , 1);
-// 		
-// 	LCD_writestr("HAWARY Sloiman");
-// 	
-//  	TMR0_delayms(2000);
-//  	
-//  	LCD_sendcmd(LCD_CLEAR);
-// 	 
-// 	 TMR0_delayms(20);
-// 	
-//  	LCD_writestr("Welcome !");
-// 		
-//  	TMR0_delayms(2000);	
-// 	 
-// 	 LCD_sendcmd(LCD_CLEAR);
-// 	 
-// 	 TMR0_delayms(20);
+
 	
 }
