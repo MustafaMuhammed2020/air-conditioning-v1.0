f5db23dffe308e85dcf2ee288c80f5d88f49a33f
Anas <67676102+AnasMahmoud99@users.noreply.github.com>
2023-04-26 22:36:06 +0200

    Update KEYPAD_program.c

--- air conditioning/HAL/keypad/KEYPAD_program.c
+++ air conditioning/HAL/keypad/KEYPAD_program.c
@@ -10,8 +10,6 @@
 #include "KEYPAD_interface.h"
 #include "KEYPAD_private.h"
 
-/** INCLUDE LIBRARIES **/
-#include "KEYPAD_interface.h"
 
 uint8_t Col_Arr[3] = {COL_1, COL_2, COL_3};
 uint8_t Row_Arr[3] = {ROW_1, ROW_2, ROW_3};
@@ -27,14 +25,14 @@ uint8_t Button_Arr[3][3] =
 void KEYPAD_init(void)
 {
   /* Set Columns direction to Output */
-  DIO_setpindir(DIO_PORTC, COL_1, DIO_PIN_INPUT);
-  DIO_setpindir(DIO_PORTC, COL_2, DIO_PIN_INPUT);
-  DIO_setpindir(DIO_PORTC, COL_3, DIO_PIN_INPUT);
+  DIO_setpindir(DIO_PORTC, COL_1, DIO_PIN_OUTPUT);
+  DIO_setpindir(DIO_PORTC, COL_2, DIO_PIN_OUTPUT);
+  DIO_setpindir(DIO_PORTC, COL_3, DIO_PIN_OUTPUT);
 
   /* Set Rows direction to Input */
-  DIO_setpindir(DIO_PORTC, ROW_1, DIO_PIN_OUTPUT);
-  DIO_setpindir(DIO_PORTC, ROW_2, DIO_PIN_OUTPUT);
-  DIO_setpindir(DIO_PORTC, ROW_3, DIO_PIN_OUTPUT);
+  DIO_setpindir(DIO_PORTA, ROW_1, DIO_PIN_INPUT);
+  DIO_setpindir(DIO_PORTA, ROW_2, DIO_PIN_INPUT);
+  DIO_setpindir(DIO_PORTA, ROW_3, DIO_PIN_INPUT);
 
   /*Set Column Initial Values to 1 */
   DIO_setpinvalue(DIO_PORTC, COL_1, DIO_PIN_HIGH);
@@ -42,35 +40,42 @@ void KEYPAD_init(void)
   DIO_setpinvalue(DIO_PORTC, COL_3, DIO_PIN_HIGH);
 
   /* Enable Pull Up Resistors at Input Rows */
-  DIO_setpinvalue(DIO_PORTC, ROW_1, DIO_PIN_HIGH);
-  DIO_setpinvalue(DIO_PORTC, ROW_2, DIO_PIN_HIGH);
-  DIO_setpinvalue(DIO_PORTC, ROW_3, DIO_PIN_HIGH);
+  DIO_setpinvalue(DIO_PORTA, ROW_1, DIO_PIN_HIGH);
+  DIO_setpinvalue(DIO_PORTA, ROW_2, DIO_PIN_HIGH);
+  DIO_setpinvalue(DIO_PORTA, ROW_3, DIO_PIN_HIGH);
 }
 
 uint8_t KEYPAD_getpressedkey(void)
 {
 
   uint8_t colValue;
+  
+  for (uint8_t counter_col = 0; counter_col < KEYPADSIZE; counter_col++)
+  {
+	  // set current pin in ColumnArray HIGH
+	  DIO_setpinvalue(DIO_PORTC, Col_Arr[counter_col], DIO_PIN_HIGH);
+  }
 
   // Scan all keypad columns
-  for (uint8_t counter_row = 0; counter_row < KEYPADSIZE; counter_row++)
+for (uint8_t counter_col = 0; counter_col < KEYPADSIZE; counter_col++)
   {
-    // activate current colmun
-    DIO_setpinvalue(DIO_PORTC, Row_Arr[counter_row], DIO_PIN_LOW);
-
-    for (uint8_t counter_col = 0; counter_col < KEYPADSIZE; counter_col++)
+    // set current column pin low to test all row pins to find out which button has been pressed
+    DIO_setpinvalue(DIO_PORTC, Col_Arr[counter_col], DIO_PIN_LOW);
+	
+	//scan and test all keypad row pins
+     for (uint8_t counter_row = 0; counter_row < KEYPADSIZE; counter_row++)
     {
       // check row value if it is 0 then this column and row is connected
-      DIO_readpin(DIO_PORTC, Col_Arr[counter_col], &colValue);
+      DIO_readpin(DIO_PORTA, Row_Arr[counter_row], &colValue);
       if (colValue == 0)
       {
         // Return pressed button value
-        DIO_setpinvalue(DIO_PORTC, Row_Arr[counter_row], DIO_PIN_HIGH);
+        DIO_setpinvalue(DIO_PORTA, Row_Arr[counter_row], DIO_PIN_HIGH);
         return Button_Arr[counter_row][counter_col];
       }
     }
     // set current column pin HIGH again
-    DIO_setpinvalue(DIO_PORTC, Row_Arr[counter_row], DIO_PIN_HIGH);
+    DIO_setpinvalue(DIO_PORTC, Col_Arr[counter_col], DIO_PIN_HIGH);
   }
 
   // Report no Button pressed
